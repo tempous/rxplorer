14be1b1d021a7bc37aafebc7741932b81321c586
tempous <tsvetkovigor5530@gmail.com>
2022-01-04 00:42:55 +0300

    Add project files

---
+++ LiteExplorer/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LiteExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
