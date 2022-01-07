c077fda0e2a6222b335e052bb07cf42c2f7a57c0
tempous <tsvetkovigor5530@gmail.com>
2022-01-07 03:25:25 +0300

    Base ViewModel class

---
+++ LiteExplorer/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LiteExplorer.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
