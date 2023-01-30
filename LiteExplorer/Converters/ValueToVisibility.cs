3964eb46eaaebea80a97e27608f72c705e565f14
tempous <tsvetkovigor5530@gmail.com>
2023-01-30 05:31:36 +0300

    add: Open directory with indicator

---
+++ LiteExplorer/Converters/ValueToVisibility.cs
@@ -0,0 +1,15 @@
+﻿using LiteExplorer.Converters.Base;
+using System;
+using System.Globalization;
+using System.Windows;
+
+namespace LiteExplorer.Converters
+{
+    internal class ValueToVisibility : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is double percent && percent == 100.0 ? Visibility.Hidden : Visibility.Visible;
+        }
+    }
+}
