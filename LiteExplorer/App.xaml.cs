7a91f86003eacadb90ecee04dc46432c4f77fba5
tempous <tsvetkovigor5530@gmail.com>
2023-02-11 01:57:33 +0300

    add: Dependency injection

--- LiteExplorer/App.xaml.cs
+++ LiteExplorer/App.xaml.cs
@@ -1,4 +1,32 @@
-﻿namespace LiteExplorer
+﻿using LiteExplorer.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace LiteExplorer
 {
-    public partial class App { }
+    public partial class App
+    {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<MainWindow>(/*provider => new()
+            {
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
+            }*/);
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = serviceProvider.GetRequiredService<MainWindowViewModel>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
 }
